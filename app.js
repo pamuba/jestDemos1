7da0b808be682c82b8d28e22b5ab9b4bfc5469cc
pamuba <pamuba@users.noreply.github.com>
2020-07-31 21:49:13 +0530

    8

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const todoRoutes = require("./routes/todo.routes"); 
 const app = express();
+const mongodb  = require("./mongodb/mongodb.connect")
+
+
+mongodb.connect();
+
+app.use(express.json());
 
 app.use("/todos", todoRoutes)
 
@@ -8,9 +14,9 @@ app.get("/", (req, res) => {
     res.json("Hello World!!");
 });
 
-app.listen(3000, ()=>{
-    console.log("Server is running on PORT 3000")
-});
+// app.listen(3000, ()=>{
+//     console.log("Server is running on PORT 3000")
+// });
 
 
 module.exports = app;
